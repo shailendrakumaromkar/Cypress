f4252319a01476a983d2e6e9c0046712931cd82a
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-14 22:47:56 +0530

    added 1st test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,10 +1,10 @@
-
+/// <reference types = "cypress" />
 
 describe("Demo Test Suite", function(){
 
     it("1st Test Case", function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-
+        cy.get(.search-keyword).
     })
 
 })
