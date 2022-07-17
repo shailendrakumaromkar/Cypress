754c63c1b4938a5b2888a866d882c8f0f77600a0
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-17 18:39:29 +0530

    added test case

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -4,8 +4,12 @@ describe("Demo Test Suite", function(){
 
     it("1st Test Case", function(){
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','Option1') 
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1') 
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check(['option1', 'option3'])
+
+        cy.get('select').select('option2').should('have.value','option2')
+        
 
     })
 
