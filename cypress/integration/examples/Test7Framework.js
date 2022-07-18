7340a64c2bd0d5edc969c3414e50b71d850b0794
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-18 15:23:33 +0530

    added test case

---
+++ cypress/integration/examples/Test7Framework.js
@@ -0,0 +1,29 @@
+/// <reference types = "cypress" />
+
+describe("Demo Test Suite", function(){
+    before(() => {
+        // root-level hook
+        // runs once before all tests
+        cy.fixture('example').then(function(data){
+            this.data=data
+        })
+      })
+
+    it("1st Test Case", function(){
+        cy.visit("https://rahulshettyacademy.com/angularpractice/");
+        cy.get('input[name="name"]:nth-child(2)').type( this.data.name)
+        cy.get('select').select( this.data.gender)
+        cy.get(':nth-child(4) > .ng-untouched').should('have.value',this.data.name)
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength',2)
+        cy.get('#inlineRadio3').should('be.disabled')
+        cy.get(':nth-child(2) > .nav-link').click()
+
+        this.data.productName.forEach(function(element){
+            cy.selectProduct(element)
+        });
+        
+
+        
+    })
+
+})
